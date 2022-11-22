15b3e5944550cc17e6abe23717183ce412e504fe
Anna Mikhalevich <annamiha@list.ru>
2022-11-22 16:09:16 +0300

    Разработка сервера, функции с фильмом

---
+++ Server/src/mediator/Mediator.java
@@ -0,0 +1,44 @@
+package mediator;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Mediator {
+    private ServerSocket serverSocket = null;
+    public static int connectionsCounter = 0;
+
+
+    public Mediator(){
+        System.out.println("-------1-------");
+        try{
+            System.out.println("-------2-------");
+            Socket clientSocket = null;
+            try{
+                System.out.println("-------3-------");
+                serverSocket = new ServerSocket(2626);
+                System.out.println("-------4-------");
+                System.out.println("-------Сервер запущен-------");
+                while(true){
+                    clientSocket = serverSocket.accept();
+                    Runnable run = new ThreadEchoHandler(clientSocket);
+                    Thread newThread = new Thread(run);
+                    newThread.start();
+                    System.out.println("Новое подключение...\nКоличество активных подключений: " + ++connectionsCounter + "\n");
+                }
+            }
+            finally{
+                clientSocket.close();
+                serverSocket.close();
+            }
+        }
+        catch(IOException e)
+        {
+
+        }
+    }
+
+    public static void main(String args[]){
+        new Mediator();
+    }
+}
